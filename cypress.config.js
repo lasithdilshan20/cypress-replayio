9953c68e66a312735b2d5fa2d8589e067d8c8d9d
Lasitha Wijenayake <lasithdilshan20@gmail.com>
2022-12-30 11:23:41 +0800

    Add configurations
    Add configurations

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+import cypressReplay from "@replayio/cypress";
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Adds "Replay Firefox" (macOS, linux) and "Replay Chromium" (linux)
+      // browsers and hooks into Cypress lifecycle methods to capture test
+      // metadata and results
+      cypressReplay(on, config);
+      return config;
     },
   },
 });
